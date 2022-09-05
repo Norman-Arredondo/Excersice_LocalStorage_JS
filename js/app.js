ea210df1e0c79816115aca44b565ab77b33efbf1
Norman Arredondo <arredondo_norman25@outlook.es>
2022-09-05 01:02:44 -0500

    Validando el formulario
    SetTimeout para la alerta

--- js/app.js
+++ js/app.js
@@ -16,5 +16,33 @@ function eventListeners() {
 
 function agregarTweet(e) {
     e.preventDefault();
-    console.log('Agregando Tweet');
+    
+    // Textarea donde el usuario escribe 
+    const tweet = document.querySelector('#tweet').value; // Una vez que se presione submit, se lee el valor del tweet
+
+    // Validación... 
+
+    if(tweet === '') {
+        mostrarError('Un tweet no puede ir vacío');
+        return; // Evita que se ejecuten más líneas de código. Funciona en un if si está en una función
+    }
+
+    console.log('agregando tweet');
+    
+}
+
+// Mostrar mensaje de error
+function mostrarError( error ) {
+    // HTML con scripting
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error'); //Clase de CSS
+
+    // Insertarlo arriba del contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    setTimeout (() => {
+        mensajeError.remove();
+    },2000);
 }
